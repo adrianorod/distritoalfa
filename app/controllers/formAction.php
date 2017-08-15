c97e09353d75e56b261acfd970f1c2de0f5245fc
adrianorod <adriano.rodrigues@medgrupo.com.br>
2017-08-15 18:54:37 -0300

    Adicionando log para envios de formulario

--- app/controllers/formAction.php
+++ app/controllers/formAction.php
@@ -9,13 +9,11 @@
     $frm['telResponsavel'] = $_POST['telResponsavel'];
     $frm['emailResponsavel'] = $_POST['emailResponsavel'];
     $frm['objetivo'] = $_POST['objetivo'];
-    $frm['passagemDeSom'] = $_POST['passagemDeSom'];
     $frm['tempoDeApresentacao'] = $_POST['tempoDeApresentacao'];
+    $frm['passagemDeSom'] = $_POST['passagemDeSom'];
     $frm['cronograma'] = $_POST['cronograma'];
-    $frm['pedestais'] = $_POST['pedestais'];
-    $frm['pedestaisQtd'] = $_POST['pedestaisQtd'];
-    $frm['microfones'] = $_POST['microfones'];
     $frm['microfonesQtd'] = $_POST['microfonesQtd'];
+    $frm['pedestaisQtd'] = $_POST['pedestaisQtd'];
     $frm['ampBaixo'] = $_POST['ampBaixo'];
     $frm['ampGuitarra'] = $_POST['ampGuitarra'];
     $frm['canalTeclado'] = $_POST['canalTeclado'];
@@ -24,7 +22,6 @@
     $frm['pratosBateria'] = $_POST['pratosBateria'];
     $frm['caixaBateria'] = $_POST['caixaBateria'];
     $frm['obsEstrutura'] = $_POST['obsEstrutura'];
-    $frm['aceiteVenda'] = $_POST['aceiteVenda'];
 
     foreach ($frm as &$value) {
         if (empty($value) || is_null($value)) {
@@ -47,35 +44,37 @@
         <b>E-mail do respons&aacute;vel: </b>$frm[emailResponsavel]<br/>
         -------------<br/><br/>
         <b>Objetivo do evento: </b>$frm[objetivo]<br/>
-        <b>Passagem de som: </b>$frm[passagemDeSom]<br/>
         <b>Quantidade de m&uacute;sicas (ou tempo dispon&iacute;vel): </b>$frm[tempoDeApresentacao]<br/>
+        <b>Passagem de som: </b>$frm[passagemDeSom]<br/>
         <b>Cronograma e onde a banda estar&aacute; encaixada: </b>$frm[cronograma]<br/>
         -------------<br/><br/>
         <b>Estrutura dispon&iacute;vel no local</b><br/>
-        Pedestais? $frm[pedestais]. Quantos? $frm[pedestaisQtd]<br/>
-        Microfones? $frm[microfones]. Quantos? $frm[microfonesQtd]<br/>
+        Microfones? $frm[microfonesQtd]<br/>
+        Pedestais? $frm[pedestaisQtd]<br/>
         Amplificador para baixo? $frm[ampBaixo]<br/>
         Amplificador para guitarra? $frm[ampGuitarra]<br/>
         Canal para teclado? $frm[canalTeclado]<br/>
         Canal para viol&atilde;o? $frm[canalViolao]<br/>
         Bateria (bumbo, tambores e estantes para pratos)? $frm[bateria]<br/>
         Pratos para bateria? $frm[pratosBateria]<br/>
-        Caixa para bateria? $frm[caixaBateria]<br/>
-        Observa&ccedil;&otilde;es a respeito da estrutura de som? $frm[obsEstrutura]<br/>
-        -------------<br/><br/>
-        Será possível realizar a venda de produtos personalizados do Distrito Alfa? $frm[aceiteVenda]";
+        Caixa / tarol para bateria? $frm[caixaBateria]<br/>
+        Observa&ccedil;&otilde;es a respeito da estrutura de som? $frm[obsEstrutura]";
     $m['Titulo_do_Email'] = "Convite - Distrito Alfa";
     $m['Email_Destino'] = "distritoalfa01@gmail.com";
     mail($m['Email_Destino'],$m['Titulo_do_Email'],$BODY['EMAIL'],"$headers");
+
+    $fp = fopen("log_convites.txt", "a");
+    $escreve = fwrite($fp, $BODY['EMAIL']);
+    fclose($fp);
+
     echo "<!DOCTYPE html>
-        <html lang=\"pt-BR\">
+      <html lang=\"pt-BR\">
 			<head>
 			</head>
 			<body>
 				<script>
-					alert(\"Agradecemos desde ja pelo convite! Sua mensagem foi enviada com sucesso, brevemente retornaremos o contato.\");
-					window.location=\"../index.html\";
+					location.href=\"../../index.html#/agradecimentos\"
 				</script>
 			</body>
-        </html>";
+      </html>";
 ?>
