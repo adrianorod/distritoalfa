610f61a30733229347f8a3ea32dcc433249d8129
adrianorod <adriano.rodrigues@medgrupo.com.br>
2017-09-08 19:55:29 -0300

    Alterações visuais

--- dist/php/post.php
+++ dist/php/post.php
@@ -1,6 +1,9 @@
-<?
+<?php
+
+  $db = new PDO('mysql:host=localhost;dbname=distrit1_teste','distrit1_admin','afdk79plw85w');
 
   $data = json_decode(file_get_contents("php://input"));
-  echo $data->emailResponsavel;
+
+  $stmt = $db->prepare();
 
 ?>
