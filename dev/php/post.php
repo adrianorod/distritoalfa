b60b7b07fe8e96ed0cf0a189ee02b137caded4af
adrianorod <adriano.rodrigues@medgrupo.com.br>
2017-09-13 19:50:50 -0300

    Ajustes no backend

--- dev/php/post.php
+++ dev/php/post.php
@@ -4,19 +4,26 @@
 
   $data = json_decode(file_get_contents('php://input'), true);
 
-  $stmt = $db->prepare('INSERT INTO Instituicao(NomeInstituicao, TelefoneInstituicao, EmailInstituicao) VALUES(:instituicao, :telInstituicao, :emailInstituicao)');
+  $instituicao = array(':instituicao' => $data['instituicao'], ':telInstituicao' => $data['telInstituicao'], ':emailInstituicao' => $data['emailInstituicao']);
+
+  $stmt = $db->prepare('INSERT INTO INSTITUICAO(instituicao, telInstituicao, emailInstituicao) VALUES(:instituicao, :telInstituicao, :emailInstituicao)');
+  $stmt->execute($instituicao);
+
+  $stmt = $db->prepare('SELECT instituicao_id FROM INSTITUICAO WHERE INSTITUICAO.instituicao = :instituicao AND INSTITUICAO.telInstituicao = :telInstituicao AND INSTITUICAO.emailInstituicao = :emailInstituicao');
   $stmt->bindParam(':instituicao', $data['instituicao']);
   $stmt->bindParam(':telInstituicao', $data['telInstituicao']);
   $stmt->bindParam(':emailInstituicao', $data['emailInstituicao']);
   $stmt->execute();
+  $instituicao_id = $stmt->fetch(PDO::FETCH_ASSOC);
 
-  $stmt = $db->prepare('INSERT INTO Responsavel(NomeResponsavel, TelefoneResponsavel, EmailResponsavel) VALUES(:nomeResponsavel, :telResponsavel, :emailResponsavel)');
+  $stmt = $db->prepare('INSERT INTO RESPONSAVEL_INSTITUICAO(nomeResponsavel, telResponsavel, emailResponsavel, instituicao_id) VALUES(:nomeResponsavel, :telResponsavel, :emailResponsavel, :instituicao_id)');
   $stmt->bindParam(':nomeResponsavel', $data['nomeResponsavel']);
   $stmt->bindParam(':telResponsavel', $data['telResponsavel']);
   $stmt->bindParam(':emailResponsavel', $data['emailResponsavel']);
+  $stmt->bindParam(':instituicao_id', $instituicao_id);
   $stmt->execute();
 
-  $stmt = $db->prepare('INSERT INTO Evento(NomeEvento, EnderecoEvento, Data, HoraInicio, HoraTermino, SiteEvento, ObjetivoEvento, TempoApresentacao, PassagemDeSom, Cronograma) VALUES(:nomeEvento, :endereco, :data, :horaInicio, :horaTermino, :enderecoEletronico, :objetivo, :tempoDeApresentacao, :passagemDeSom, :cronograma)');
+  $stmt = $db->prepare('INSERT INTO EVENTO(nomeEvento, endereco, data, horaInicio, horaTermino, enderecoEletronico, objetivo, tempoDeApresentacao, passagemDeSom, cronograma) VALUES(:nomeEvento, :endereco, :data, :horaInicio, :horaTermino, :enderecoEletronico, :objetivo, :tempoDeApresentacao, :passagemDeSom, :cronograma)');
   $stmt->bindParam(':nomeEvento', $data['nomeEvento']);
   $stmt->bindParam(':endereco', $data['endereco']);
   $stmt->bindParam(':data', $data['data']);
@@ -27,6 +34,19 @@
   $stmt->bindParam(':tempoDeApresentacao', $data['tempoDeApresentacao']);
   $stmt->bindParam(':passagemDeSom', $data['passagemDeSom']);
   $stmt->bindParam(':cronograma', $data['cronograma']);
+  $stmt->execute();
+
+  $stmt = $db->prepare('INSERT INTO ESTRUTURA_EVENTO(microfonesQtd, pedestaisQtd, ampBaixo, ampGuitarra, canalTeclado, canalViolao, bateria, pratosBateria, caixaBateria, obsEstrutura) VALUES(:microfonesQtd, :pedestaisQtd, :ampBaixo, :ampGuitarra, :canalTeclado, :canalViolao, :bateria, :pratosBateria, :caixaBateria, :obsEstrutura)');
+  $stmt->bindParam(':microfonesQtd', $data['microfonesQtd']);
+  $stmt->bindParam(':pedestaisQtd', $data['pedestaisQtd']);
+  $stmt->bindParam(':ampBaixo', $data['ampBaixo']);
+  $stmt->bindParam(':ampGuitarra', $data['ampGuitarra']);
+  $stmt->bindParam(':canalTeclado', $data['canalTeclado']);
+  $stmt->bindParam(':canalViolao', $data['canalViolao']);
+  $stmt->bindParam(':bateria', $data['bateria']);
+  $stmt->bindParam(':pratosBateria', $data['pratosBateria']);
+  $stmt->bindParam(':caixaBateria', $data['caixaBateria']);
+  $stmt->bindParam(':obsEstrutura', $data['obsEstrutura']);
   $stmt->execute();
 
 ?>
