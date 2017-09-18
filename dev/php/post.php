811d67af6366cb473b6aa2a1faeeac5d8000bffb
adrianorod <adriano.rodrigues@medgrupo.com.br>
2017-09-18 18:50:30 -0300

    Ajustes de back-end

--- dev/php/post.php
+++ dev/php/post.php
@@ -4,49 +4,33 @@
 
   $data = json_decode(file_get_contents('php://input'), true);
 
-  $instituicao = array(':instituicao' => $data['instituicao'], ':telInstituicao' => $data['telInstituicao'], ':emailInstituicao' => $data['emailInstituicao']);
+  try {
+    $instituicao = array(':instituicao' => $data['instituicao'], ':telInstituicao' => $data['telInstituicao'], ':emailInstituicao' => $data['emailInstituicao']);
+    $stmt = $db->prepare('INSERT INTO INSTITUICAO(instituicao, telInstituicao, emailInstituicao) VALUES(:instituicao, :telInstituicao, :emailInstituicao)');
+    $stmt->execute($instituicao);
 
-  $stmt = $db->prepare('INSERT INTO INSTITUICAO(instituicao, telInstituicao, emailInstituicao) VALUES(:instituicao, :telInstituicao, :emailInstituicao)');
-  $stmt->execute($instituicao);
+    $responsavelInstituicao = array(':nomeResponsavel' => $data['nomeResponsavel'], ':telResponsavel' => $data['telResponsavel'], ':emailResponsavel' => $data['emailResponsavel']);
+    $stmt = $db->prepare('INSERT INTO RESPONSAVEL_INSTITUICAO(nomeResponsavel, telResponsavel, emailResponsavel) VALUES(:nomeResponsavel, :telResponsavel, :emailResponsavel)');
+    $stmt->execute($responsavelInstituicao);
 
-  $stmt = $db->prepare('SELECT instituicao_id FROM INSTITUICAO WHERE INSTITUICAO.instituicao = :instituicao AND INSTITUICAO.telInstituicao = :telInstituicao AND INSTITUICAO.emailInstituicao = :emailInstituicao');
-  $stmt->bindParam(':instituicao', $data['instituicao']);
-  $stmt->bindParam(':telInstituicao', $data['telInstituicao']);
-  $stmt->bindParam(':emailInstituicao', $data['emailInstituicao']);
-  $stmt->execute();
-  $instituicao_id = $stmt->fetch(PDO::FETCH_ASSOC);
-
-  $stmt = $db->prepare('INSERT INTO RESPONSAVEL_INSTITUICAO(nomeResponsavel, telResponsavel, emailResponsavel, instituicao_id) VALUES(:nomeResponsavel, :telResponsavel, :emailResponsavel, :instituicao_id)');
-  $stmt->bindParam(':nomeResponsavel', $data['nomeResponsavel']);
-  $stmt->bindParam(':telResponsavel', $data['telResponsavel']);
-  $stmt->bindParam(':emailResponsavel', $data['emailResponsavel']);
-  $stmt->bindParam(':instituicao_id', $instituicao_id);
-  $stmt->execute();
-
-  $stmt = $db->prepare('INSERT INTO EVENTO(nomeEvento, endereco, data, horaInicio, horaTermino, enderecoEletronico, objetivo, tempoDeApresentacao, passagemDeSom, cronograma) VALUES(:nomeEvento, :endereco, :data, :horaInicio, :horaTermino, :enderecoEletronico, :objetivo, :tempoDeApresentacao, :passagemDeSom, :cronograma)');
-  $stmt->bindParam(':nomeEvento', $data['nomeEvento']);
-  $stmt->bindParam(':endereco', $data['endereco']);
-  $stmt->bindParam(':data', $data['data']);
-  $stmt->bindParam(':horaInicio', $data['horaInicio']);
-  $stmt->bindParam(':horaTermino', $data['horaTermino']);
-  $stmt->bindParam(':enderecoEletronico', $data['enderecoEletronico']);
-  $stmt->bindParam(':objetivo', $data['objetivo']);
-  $stmt->bindParam(':tempoDeApresentacao', $data['tempoDeApresentacao']);
-  $stmt->bindParam(':passagemDeSom', $data['passagemDeSom']);
-  $stmt->bindParam(':cronograma', $data['cronograma']);
-  $stmt->execute();
-
-  $stmt = $db->prepare('INSERT INTO ESTRUTURA_EVENTO(microfonesQtd, pedestaisQtd, ampBaixo, ampGuitarra, canalTeclado, canalViolao, bateria, pratosBateria, caixaBateria, obsEstrutura) VALUES(:microfonesQtd, :pedestaisQtd, :ampBaixo, :ampGuitarra, :canalTeclado, :canalViolao, :bateria, :pratosBateria, :caixaBateria, :obsEstrutura)');
-  $stmt->bindParam(':microfonesQtd', $data['microfonesQtd']);
-  $stmt->bindParam(':pedestaisQtd', $data['pedestaisQtd']);
-  $stmt->bindParam(':ampBaixo', $data['ampBaixo']);
-  $stmt->bindParam(':ampGuitarra', $data['ampGuitarra']);
-  $stmt->bindParam(':canalTeclado', $data['canalTeclado']);
-  $stmt->bindParam(':canalViolao', $data['canalViolao']);
-  $stmt->bindParam(':bateria', $data['bateria']);
-  $stmt->bindParam(':pratosBateria', $data['pratosBateria']);
-  $stmt->bindParam(':caixaBateria', $data['caixaBateria']);
-  $stmt->bindParam(':obsEstrutura', $data['obsEstrutura']);
-  $stmt->execute();
+    $evento = array(':nomeEvento' => $data['nomeEvento'], ':endereco' => $data['endereco'], ':data' => $data['data'], ':horaInicio' => $data['horaInicio'], ':horaTermino' => $data['horaTermino'], ':enderecoEletronico' => $data['enderecoEletronico'], ':objetivo' => $data['objetivo'], ':tempoDeApresentacao' => $data['tempoDeApresentacao'], ':passagemDeSom' => $data['passagemDeSom'], ':cronograma' => $data['cronograma'], ':microfonesQtd' => $data['microfonesQtd'], ':pedestaisQtd' => $data['pedestaisQtd'], ':ampBaixo' => $data['ampBaixo'], ':ampGuitarra' => $data['ampGuitarra'], ':canalTeclado' => $data['canalTeclado'], ':canalViolao' => $data['canalViolao'], ':bateria' => $data['bateria'], ':pratosBateria' => $data['pratosBateria'], ':caixaBateria' => $data['caixaBateria'], ':obsEstrutura' => $data['obsEstrutura']);
+    $stmt = $db->prepare('INSERT INTO EVENTO(nomeEvento, endereco, data, horaInicio, horaTermino, enderecoEletronico, objetivo, tempoDeApresentacao, passagemDeSom, cronograma, microfonesQtd, pedestaisQtd, ampBaixo, ampGuitarra, canalTeclado, canalViolao, bateria, pratosBateria, caixaBateria, obsEstrutura) VALUES(:nomeEvento, :endereco, :data, :horaInicio, :horaTermino, :enderecoEletronico, :objetivo, :tempoDeApresentacao, :passagemDeSom, :cronograma, :microfonesQtd, :pedestaisQtd, :ampBaixo, :ampGuitarra, :canalTeclado, :canalViolao, :bateria, :pratosBateria, :caixaBateria, :obsEstrutura)');
+    $stmt->execute($evento);
+    /*
+    $instituicao = array(':instituicao' => 'instituicao teste', ':telInstituicao' => 'telefone institut teste', ':emailInstituicao' => 'imeil@inst');
+    $stmt = $db->prepare('SELECT instituicao_id FROM INSTITUICAO WHERE INSTITUICAO.instituicao = :instituicao AND INSTITUICAO.telInstituicao = :telInstituicao AND INSTITUICAO.emailInstituicao = :emailInstituicao');
+    $stmt->execute($instituicao);
+    $instituicao_id = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo $instituicao_id['instituicao_id'];
+    */
+  } catch(Exception $e) {
+    log_exception($e);
+    $salvando = json_encode($data);
+    $fp = fopen("log_convites_error.txt", "a");
+    $escreve = fwrite($fp, $salvando);
+    fclose($fp);
+  } finally {
+    //nada por enquanto
+  }
 
 ?>
