daa0d0be3b50e43ffca342cf295c939fdfcd5b89
Adriano Azevedo Rodrigues <adriano.rodrigues@medbarra.com.br>
2017-12-20 19:52:00 -0200

    ajustes no sql para resgatar dados do bd

--- dev/php/get.php
+++ dev/php/get.php
@@ -2,7 +2,7 @@
 
   require_once("db.php");
 
-  $stmt = $db->prepare('SELECT Instituicao.*, Responsavel.*, Evento.* FROM Instituicao INNER JOIN Evento ON Instituicao.ID = Evento.ID INNER JOIN Responsavel ON Instituicao.ID = Responsavel.ID');
+  $stmt = $db->prepare('SELECT I.*, R.*, E.* FROM INSTITUICAO AS I INNER JOIN EVENTO AS E ON I.instituicao_id = E.evento_id INNER JOIN RESPONSAVEL_INSTITUICAO AS R ON I.instituicao_id = R.responsavel_instituicao_id');
   $stmt->execute();
 
   $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
